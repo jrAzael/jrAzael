587d21297eb4c73a858ac6bdfde60367247a9bb5
jrAzael <160097480+jrAzael@users.noreply.github.com>
2024-02-27 21:44:28 -0600

    Add files via upload

---
+++ calculo2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (){
+    printf ("el resultado es : %f",5.0+2.0/(1.0/(1.0+2.0/(2.0-1.0/(4.0)))));
+
+}
---
+++ notas.c
@@ -0,0 +1,21 @@
+/* Programa: Nota media de tres exámenes  */
+#include <stdio.h>
+
+int main()
+{
+    float nota1, nota2, nota3, media;
+
+    printf( "\n   Introduzca nota del primer examen: " );
+    scanf( "%f", &nota1 );
+    printf( "\n   Introduzca nota del segundo examen: " );
+    scanf( "%f", &nota2 );
+    printf( "\n   Introduzca nota del tercer examen: " );
+    scanf( "%f", &nota3 );
+
+    media = ( nota1 + nota2 + nota3 ) / 3;
+
+    printf( "\n   La nota media es: %.2f", media );
+    printf("\n");
+
+    return 0;
+}
