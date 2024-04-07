38ad8437837b67388fc954a5560b87c107b8f907
jrAzael <160097480+jrAzael@users.noreply.github.com>
2024-04-06 18:38:02 -0600

    Add files via upload

--- ejer3.c
+++ ejer3.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main (){
-    int i =99;
-    int arre[100];
-    while (i>=0)
-    {
-      printf("mi arreglo [%d] \n",i);
-      i--;
+
+
+    int dato=3;
+    int res;
+    int i;
+     // scanf("%d",&dato);
+    printf("A   A+2   A+4   A+6 \n");
+    for (i=0; i<dato;i++){ 
+    printf("%d   %d     %d     %d\n",dato,dato+2,dato+4,dato+6);
     }
-    
-}
