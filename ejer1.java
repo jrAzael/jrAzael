6cfd821fb2890330ad76f34c05676aace1647984
jrAzael <160097480+jrAzael@users.noreply.github.com>
2024-04-19 23:31:31 -0600

    Add files via upload

---
+++ ejer1.java
@@ -0,0 +1,7 @@
+package git.cursoJS;
+public class ejer1{
+public static void main (String args[]){
+    System.out.println("Esta es la salida de mi programa");
+    System.out.println ("segundalinea de texto");
+}
+} 
